a23101b39b157192d96ac2d1019367924b607355
Weickmam Ferreira <weickmam07@gmail.com>
2022-02-14 17:32:41 -0300

    Projeto configurado

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,12 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader'
-      }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
     ]
   }
 }
